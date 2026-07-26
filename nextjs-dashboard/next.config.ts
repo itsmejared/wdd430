f2f8bd93a8ddaf1e008e779a90cdbfc64ca8dc9e
Jared Huayta <ejhuayta@gmail.com>
2026-07-26 15:32:54 -0300

    exclude devindicator

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Desactiva por completo el indicador flotante en desarrollo
+  devIndicators: false,
 };
 
 export default nextConfig;
